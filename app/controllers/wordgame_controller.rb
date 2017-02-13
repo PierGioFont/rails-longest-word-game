77477816a49876d585d29772d29904b7e1c3f7e9
PierGiovanni Fontana <piergiovanni@cegetel.net>
2017-02-13 17:50:23 +0100

    word game done with lot of difficult

---
+++ app/controllers/wordgame_controller.rb
@@ -0,0 +1,67 @@
+class WordgameController < ApplicationController
+  def game
+    @grid = generate_grid(20)
+  end
+
+  def score
+    start_time = params[:start_time]
+    @attempt = params[:query]
+    grid = params[:grid]
+    end_time = Time.now.to_f
+    @result = run_game(@attempt, grid.split(" "), start_time.to_f, end_time)
+  end
+
+  private
+
+  def generate_grid(size)
+    Array.new(size) { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    (time_taken > 60.0) ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    result = { time: end_time - start_time }
+    result[:time] = result[:time].round(2)
+    result[:translation] = get_translation(attempt)
+    result[:score], result[:message] = score_and_message(
+      attempt, result[:translation], grid, result[:time])
+
+    result
+  end
+
+  def score_and_message(attempt, translation, grid, time)
+    if included?(attempt.upcase, grid)
+      if translation
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def get_translation(word)
+    api_key = "c34a2381-2143-426f-ae49-0afc7ece733b"
+    begin
+      response = open("https://api-platform.systran.net/translation/text/translate?source=en&target=fr&key=#{api_key}&input=#{word}")
+      json = JSON.parse(response.read.to_s)
+      if json['outputs'] && json['outputs'][0] && json['outputs'][0]['output'] && json['outputs'][0]['output'] != word
+        return json['outputs'][0]['output']
+      end
+    rescue
+      if File.read('/usr/share/dict/words').upcase.split("\n").include? word.upcase
+        return word
+      else
+        return nil
+      end
+    end
+  end
+end
